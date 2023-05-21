cd991f0f61d297e53ff143b54f33643c04b095c5
sam <sam7.yeh@advantech.com.tw>
2023-05-21 15:01:47 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/KuanHsienYEH/vue-chart.git master:gh-pages
 
 cd -
